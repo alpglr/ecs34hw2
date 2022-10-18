e7ba9826e4b1f909278544e0872958ac4f971178
jfywang <jfywang@ucdavis.edu>
2022-10-18 16:23:50 -0700

    Adding more stuff to XMLWriter.

--- src/XMLWriter.cpp
+++ src/XMLWriter.cpp
@@ -1,8 +1,21 @@
 #include "XMLWriter.h"
-#include <expat.h>
+#include <deque>
 
 struct CXMLWriter::SImplementation {
+	std::shared_ptr<CDataSink> DSink;
+	std::deque<SXMLEntity> DEntities;
 
+	SImplementation(std::shared_ptr < CDataSink > sink) {
+		DSink = sink;
+	}
+
+	~SImplementation() {
+
+	}
+
+	bool Flush() {
+
+	}
 };
 
 CXMLWriter::CXMLWriter(std::shared_ptr< CDataSink > sink) {
@@ -11,4 +24,12 @@ CXMLWriter::CXMLWriter(std::shared_ptr< CDataSink > sink) {
 
 CXMLWriter::~CXMLWriter() {
 
+}
+
+bool Flush() {
+	return DImplementation->Flush();
+}
+
+bool WriteEntity(const SXMLEntity &entity) {
+	return DImplementation->WriteEntity(&entity);
 }
