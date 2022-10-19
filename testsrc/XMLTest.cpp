f12e4d003016ae70c8f0f67f50694b67a0f6d0a2
jfywang <jfywang@ucdavis.edu>
2022-10-19 02:52:11 -0700

    Added test that tests both XMLReader & XMLWriter with the same XML values.

--- testsrc/XMLTest.cpp
+++ testsrc/XMLTest.cpp
@@ -87,29 +87,29 @@ TEST(XMLWriter, WriteEntityTest) {
 	std::shared_ptr<CStringDataSink> Sink1 = std::make_shared<CStringDataSink>();
 	CXMLWriter Writer1(Sink1);
 
-	SXMLEntity Entity1 = {SXMLEntity::EType::StartElement, "test"};
+	SXMLEntity Entity1 = { SXMLEntity::EType::StartElement, "test" };
 	Writer1.WriteEntity(Entity1);
 	EXPECT_EQ(Sink1->String(), "<test>");
 	SXMLEntity Text1 = { SXMLEntity::EType::CharData, "test string" };
 	Writer1.WriteEntity(Text1);
-	EXPECT_EQ(Sink1->String(),	"<test>"
-									"test string");
+	EXPECT_EQ(Sink1->String(), "<test>"
+		"test string");
 	SXMLEntity Complete1 = { SXMLEntity::EType::CompleteElement, "complete" };
 	Complete1.SetAttribute("warriors", "good");
 	Complete1.SetAttribute("lakers", "bad");
 	Writer1.WriteEntity(Complete1);
-	EXPECT_EQ(Sink1->String(),	"<test>"
-									"test string"
-									"<complete warriors=\"good\" lakers=\"bad\"/>");
+	EXPECT_EQ(Sink1->String(), "<test>"
+		"test string"
+		"<complete warriors=\"good\" lakers=\"bad\"/>");
 	Entity1.DType = SXMLEntity::EType::EndElement;
 	Writer1.WriteEntity(Entity1);
-	EXPECT_EQ(Sink1->String(),	"<test>"
-									"test string"
-									"<complete warriors=\"good\" lakers=\"bad\"/>"
-								"</test>");
-	
+	EXPECT_EQ(Sink1->String(), "<test>"
+		"test string"
+		"<complete warriors=\"good\" lakers=\"bad\"/>"
+		"</test>");
+}
 
-
+TEST(XMLWriter, FlushTest) {
 	std::shared_ptr<CStringDataSink> Sink2 = std::make_shared<CStringDataSink>();
 	CXMLWriter Writer2(Sink2);
 
@@ -131,4 +131,95 @@ TEST(XMLWriter, WriteEntityTest) {
 										"</test4>"
 									"</test3>"
 								"</test2>");
+}
+
+TEST(XMLWriter, ReadAndWriteTest) {
+	std::shared_ptr<CStringDataSink> Sink = std::make_shared<CStringDataSink>();
+	CXMLWriter Writer(Sink);
+
+	SXMLEntity ArtistStart = { SXMLEntity::EType::StartElement, "artist" };
+	SXMLEntity ArtistName = { SXMLEntity::EType::CharData, "Bladee" };
+	SXMLEntity AlbumStart = { SXMLEntity::EType::StartElement, "album" };
+	SXMLEntity AlbumEnd = { SXMLEntity::EType::EndElement, "album" };
+	SXMLEntity ReleaseDate = { SXMLEntity::EType::CompleteElement, "release-date" };
+
+	Writer.WriteEntity(ArtistStart);
+	Writer.WriteEntity(ArtistName);
+	AlbumStart.SetAttribute("name", "Eversince");
+	ReleaseDate.SetAttribute("date", "05/25/2016");
+	Writer.WriteEntity(AlbumStart);
+	Writer.WriteEntity(ReleaseDate);
+	Writer.WriteEntity(AlbumEnd);
+	AlbumStart.SetAttribute("name", "Icedancer");
+	ReleaseDate.SetAttribute("date", "12/29/2018");
+	Writer.WriteEntity(AlbumStart);
+	Writer.WriteEntity(ReleaseDate);
+	Writer.WriteEntity(AlbumEnd);
+	AlbumStart.SetAttribute("name", "The Fool");
+	ReleaseDate.SetAttribute("date", "05/28/2021");
+	Writer.WriteEntity(AlbumStart);
+	Writer.WriteEntity(ReleaseDate);
+	Writer.Flush();
+
+	EXPECT_EQ(Sink->String(),	"<artist>Bladee"
+									"<album name=\"Eversince\">"
+										"<release-date date=\"05/25/2016\"/>"
+									"</album>"
+									"<album name=\"Icedancer\">"
+										"<release-date date=\"12/29/2018\"/>"
+									"</album>"
+									"<album name=\"The Fool\">"
+										"<release-date date=\"05/28/2021\"/>"
+									"</album>"
+								"</artist>");
+
+	CStringDataSource Source(Sink->String());
+	std::shared_ptr<CStringDataSource> PSource = std::make_shared<CStringDataSource>(Source);
+	CXMLReader Reader(PSource);
+
+	SXMLEntity NextEntity;
+	Reader.ReadEntity(NextEntity);
+	EXPECT_EQ(NextEntity.DType, ArtistStart.DType);
+	EXPECT_EQ(NextEntity.DNameData, ArtistStart.DNameData);
+	EXPECT_FALSE(NextEntity.AttributeExists("artist"));
+	Reader.ReadEntity(NextEntity);
+	EXPECT_EQ(NextEntity.DType, ArtistName.DType);
+	EXPECT_EQ(NextEntity.DNameData, ArtistName.DNameData);
+	Reader.ReadEntity(NextEntity);
+	EXPECT_EQ(NextEntity.DType, AlbumStart.DType);
+	EXPECT_EQ(NextEntity.DNameData, AlbumStart.DNameData);
+	EXPECT_EQ(NextEntity.AttributeValue("name"), "Eversince");
+	Reader.ReadEntity(NextEntity);
+	EXPECT_EQ(NextEntity.DType, SXMLEntity::EType::StartElement);
+	EXPECT_EQ(NextEntity.DNameData, ReleaseDate.DNameData);
+	EXPECT_EQ(NextEntity.AttributeValue("date"), "05/25/2016");
+	Reader.ReadEntity(NextEntity);
+	EXPECT_EQ(NextEntity.DType, SXMLEntity::EType::EndElement);
+	EXPECT_EQ(NextEntity.DNameData, ReleaseDate.DNameData);
+	Reader.ReadEntity(NextEntity);
+	EXPECT_EQ(NextEntity.DType, AlbumEnd.DType);
+	EXPECT_EQ(NextEntity.DNameData, AlbumEnd.DNameData);
+	Reader.ReadEntity(NextEntity);
+	Reader.ReadEntity(NextEntity);
+	EXPECT_FALSE(Reader.End());
+	Reader.ReadEntity(NextEntity);
+	Reader.ReadEntity(NextEntity);
+	Reader.ReadEntity(NextEntity);
+	EXPECT_EQ(NextEntity.DType, AlbumStart.DType);
+	EXPECT_EQ(NextEntity.DNameData, AlbumStart.DNameData);
+	EXPECT_EQ(NextEntity.AttributeValue("name"), AlbumStart.AttributeValue("name"));
+	Reader.ReadEntity(NextEntity);
+	EXPECT_EQ(NextEntity.DType, SXMLEntity::EType::StartElement);
+	EXPECT_EQ(NextEntity.DNameData, ReleaseDate.DNameData);
+	EXPECT_EQ(NextEntity.AttributeValue("date"), ReleaseDate.AttributeValue("date"));
+	Reader.ReadEntity(NextEntity);
+	EXPECT_EQ(NextEntity.DType, SXMLEntity::EType::EndElement);
+	EXPECT_EQ(NextEntity.DNameData, ReleaseDate.DNameData);
+	Reader.ReadEntity(NextEntity);
+	EXPECT_EQ(NextEntity.DType, AlbumEnd.DType);
+	EXPECT_EQ(NextEntity.DNameData, AlbumEnd.DNameData);
+	Reader.ReadEntity(NextEntity);
+	EXPECT_EQ(NextEntity.DType, SXMLEntity::EType::EndElement);
+	EXPECT_EQ(NextEntity.DNameData, "artist");
+	EXPECT_TRUE(Reader.End());
 }
