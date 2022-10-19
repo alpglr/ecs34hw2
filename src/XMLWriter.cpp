01ab3aa932a4183e1955b6563fd9c4e835d69002
jfywang <jfywang@ucdavis.edu>
2022-10-18 18:25:45 -0700

    Rough draft of XMLWriter.

--- src/XMLWriter.cpp
+++ src/XMLWriter.cpp
@@ -1,9 +1,9 @@
 #include "XMLWriter.h"
-#include <deque>
+#include <stack>
 
 struct CXMLWriter::SImplementation {
 	std::shared_ptr<CDataSink> DSink;
-	std::deque<SXMLEntity> DEntities;
+	std::stack<SXMLEntity> DEntities;
 
 	SImplementation(std::shared_ptr < CDataSink > sink) {
 		DSink = sink;
@@ -14,7 +14,48 @@ struct CXMLWriter::SImplementation {
 	}
 
 	bool Flush() {
+		while (DEntities.top()) {
+			WriteEntity(DEntities.top());
+			DEntities.pop();
+		}
 
+		return true;
+	}
+
+	bool WriteEntity(const SXMLEntity& entity) {
+		switch (entity.DType) {
+		case (SXMLEntity::EType::StartElement):
+			DSink->Write("<" + entity.DNameData);
+			for (auto& Attribute : entity.DAttributes) {
+				DSink->Write(" " + std::get<0>(Attribute) + "=\"" + std::get<1>(Attribute) + "\"");
+			}
+			DSink->Put('>');
+
+			SXMLEntity entity_copy = { .DType = SXMLEntity::EType::EndElement, .DNameData = entity.DNameData, 
+										.DAttributes = entity.DAttributes};
+			DEntities.push(entity_copy);
+			break;
+		case (SXMLEntity::EType::EndElement):
+			if (DEntities.top().DNameData == entity.DNameData) {
+				DSink->Write("</" + entity.DNameData + ">");
+				DEntities.pop();
+				break;
+			}
+			else {
+				return false;
+			}
+		case (SXMLEntity::EType::CharData):
+			DSink->Write(entity.DNameData);
+			break;
+		case (SXMLEntity::EType::CompleteElement):
+			DSink->Write("<" + entity.DNameData);
+			for (auto& Attribute : entity.DAttributes) {
+				DSink->Write(" " + std::get<0>(Attribute) + "=\"" + std::get<1>(Attribute) + "\"");
+			}
+			DSink->Write("/>");
+		}
+
+		return true;
 	}
 };
 
